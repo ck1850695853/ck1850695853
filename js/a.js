7523c44a66cdb6e947dfa3ecf391fbc519fc0880
ck <1850695853@qq.com>
2020-12-31 18:13:01 +0800

    blog

---
+++ js/a.js
@@ -0,0 +1,5 @@
+alert(1);
+console.log('a.js')
+var fn=()=>{
+    console.log('我是箭头函数');
+}
---
+++ js/b.js
@@ -0,0 +1,7 @@
+var a=()=>{console.log(1)};
+var b=()=>{console.log(2)};
+var obj={
+    c:1,
+    d:2
+}
+let {c,d}=obj;
